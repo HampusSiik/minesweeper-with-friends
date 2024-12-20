a328f56ff4d43fa376d952a179c40fa233ae2bc4
Hampus Siik <hampus.siik@gmail.com>
2024-12-20 09:49:02 +0100

    Redid main and added documentation to board.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+
 from minesweeper import board
 
 
 def main():
     b = board.Board()
-    b.generate_board(5, 5, 5)
+    b.generate_board(5, 5)
+    b.place_mines(5, (0, 0))
     print(b.show_board())
 
 
--- src/minesweeper/board.py
+++ src/minesweeper/board.py
@@ -1,21 +1,45 @@
-from typing import List, Tuple
+from typing import List, Tuple, Generator
 import random
 
 from minesweeper.cells.cellcontainer import CellContainer
 
-
+Position = Tuple[int, int]
 
 class Board:
+    """
+    Minesweeper board.
+    """
 
     _board: List[List[CellContainer]]
 
     def __init__(self):
+        """
+        Initialize the board.
+        """
         self._board = [[]]
 
     def generate_board(self, width: int, height: int) -> None:
+        """
+        Generate an empty board with the given dimensions.
+
+        Args:
+            width (int): Width of the board.
+            height (int): Height of the board.
+        """
         self._board = [[CellContainer.create_empty() for _ in range(width)] for _ in range(height)]
 
-    def place_mines(self, mines: int, start_position: Tuple[int, int]) -> None:
+    def place_mines(self, mines: int, start_position: Position) -> None:
+        """
+        Place mines on the board.
+
+        Does not place mines on the start position.
+
+        Does not avoid existing mines.
+
+        Args:
+            mines (int): Number of mines to place.
+            start_position (Position): Position where the first cell was clicked.
+        """
         positions = [(x, y) for x in range(len(self._board)) for y in range(len(self._board[0]))]
         positions.remove(start_position)
         mines_positions = random.sample(positions, mines)
@@ -23,4 +47,33 @@ class Board:
             self._board[x][y] = CellContainer.create_mine()
 
     def show_board(self) -> str:
+        """
+        String representation of the board.
+
+        Returns:
+            str: String representation of the board.
+        """
         return "\n".join(["".join([str(cell.unwraped()) for cell in row]) for row in self._board])
+
+    def surrounding_mines(self, position: Position) -> int:
+        """
+        Number of mines surrounding a position.
+
+        Args:
+            position (Position): Position to check.
+        Returns:
+            int: Number of mines surrounding the position.
+        """
+        return sum([self._board[i][j].is_mine() for i, j in self._bounded_range(position)])
+
+    def _bounded_range(self, position: Position) -> Generator[Position]:
+        """
+        Range of positions around position that are within the board.
+
+        Args:
+            position (Position): Position to get the range around.
+        Returns:
+            Generator[Position]: List of positions around position that are within the board.
+        """
+        x, y = position
+        return ((i, j) for i in range(max(0, x - 1), min(len(self._board), x + 2)) for j in range(max(0, y - 1), min(len(self._board[0]), y + 2)))
