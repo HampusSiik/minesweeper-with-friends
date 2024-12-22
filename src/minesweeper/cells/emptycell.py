6d012dff55babe41973249aee854ccc0a30c5a30
Hampus Siik <hampus.siik@gmail.com>
2024-12-22 18:52:08 +0100

    Refactor EmptyCell class to inherit from MineCell and add docstring for improved clarity

--- src/minesweeper/cells/emptycell.py
+++ src/minesweeper/cells/emptycell.py
@@ -1,29 +1,25 @@
-"""Empty cell class"""
-
-from minesweeper.cells.cell import Cell
-from minesweeper.cells.flaggedcell import FlaggedCell
-from minesweeper.cells.revealedcell import RevealedCell
+from minesweeper.cells.minecell import MineCell
 
 
-class EmptyCell:
+class EmptyCell(MineCell):
+    """
+    Represents an empty cell in the minesweeper game.
+    """
 
     def is_mine(self) -> bool:
+        """
+        Check if the cell is a mine.
+
+        Returns:
+            bool: False
+        """
         return False
-
-    def is_flagged(self) -> bool:
-        return False
-
-    def is_revealed(self) -> bool:
-        return False
-
-    def unwraped(self) -> Cell:
-        return self
-
-    def flagged(self) -> Cell:
-        return FlaggedCell(self)
-
-    def revealed(self) -> Cell:
-        return RevealedCell(self)
 
     def __str__(self) -> str:
+        """
+        Get the string representation of the cell.
+
+        Returns:
+            str: "E"
+        """
         return "E"
