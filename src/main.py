d3227ee10a02406f10c125c7d3a23fabb6900e2d
Hampus Siik <hampus.siik@gmail.com>
2024-12-22 13:19:40 +0100

    Update main to display nearby mines instead of the full board

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     b = board.Board()
     b.generate_board(5, 5)
     b.place_mines(5, (0, 0))
-    print(b.show_board())
+    print(b.show_nearby_mines())
 
 
 if __name__ == "__main__":
