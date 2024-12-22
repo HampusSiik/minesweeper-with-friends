0ca49ced3804451e47625293999eed364253e084
Hampus Siik <hampus.siik@gmail.com>
2024-12-22 15:08:40 +0100

    Add docstrings to RevealedCell methods for improved documentation

--- src/minesweeper/cells/revealedcell.py
+++ src/minesweeper/cells/revealedcell.py
@@ -2,29 +2,77 @@ from minesweeper.cells.cell import Cell
 
 
 class RevealedCell:
+    """
+    Represents a revealed cell in the minesweeper game.
+    """
 
     _cell: Cell
 
     def __init__(self, cell: Cell):
+        """
+        Initializes a new instance of the RevealedCell class.
+        """
         self._cell = cell
 
     def is_mine(self) -> bool:
+        """
+        Check if the cell is a mine.
+
+        Returns:
+            bool: True if the cell is a mine, False otherwise
+        """
         return self._cell.is_mine()
 
     def is_flagged(self) -> bool:
+        """
+        Check if the cell is flagged.
+
+        Returns:
+            bool: False
+        """
         return False
 
     def is_revealed(self) -> bool:
+        """
+        Check if the cell is revealed.
+
+        Returns:
+            bool: True
+        """
         return True
 
     def unwraped(self) -> Cell:
+        """
+        Unwrap the cell.
+
+        Returns:
+            Cell: The unwrapped cell.
+        """
         return self._cell.unwraped()
 
     def flagged(self) -> Cell:
+        """
+        Flag the cell.
+
+        Returns:
+            Cell: The contained cell flagged.
+        """
         return self._cell.flagged()
 
     def revealed(self) -> Cell:
+        """
+        Reveal the cell.
+
+        Returns:
+            Cell: The contained cell revealed.
+        """
         return self
 
     def __str__(self) -> str:
+        """
+        String representation of the cell.
+
+        Returns:
+            str: "R"
+        """
         return "R"
