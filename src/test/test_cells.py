9b48d435f21cb9311a988cbe4c00e6cc0b60ef58
Hampus Siik <hampus.siik@gmail.com>
2024-12-22 13:24:53 +0100

    Add unit tests for EmptyCell methods in test_cells.py

--- src/test/test_cells.py
+++ src/test/test_cells.py
@@ -1,6 +1,13 @@
 import unittest
 
-from minesweeper.cells import emptycell
+from minesweeper.cells import (
+    emptycell,
+    minecell,
+    cellcontainer,
+    cell,
+    revealedcell,
+    flaggedcell,
+)
 
 
 class TestEmptyCell(unittest.TestCase):
@@ -11,3 +18,23 @@ class TestEmptyCell(unittest.TestCase):
             self.assertTrue(True)
         except Exception:
             self.fail("Could not create EmptyCell")
+
+    def test_is_mine(self):
+        ec = emptycell.EmptyCell()
+        self.assertFalse(ec.is_mine())
+
+    def test_is_flagged(self):
+        ec = emptycell.EmptyCell()
+        self.assertFalse(ec.is_flagged())
+
+    def test_is_revealed(self):
+        ec = emptycell.EmptyCell()
+        self.assertFalse(ec.is_revealed())
+
+    def test_flagged(self):
+        ec = emptycell.EmptyCell()
+        self.assertTrue(ec.flagged().is_flagged())
+
+    def test_unwrap(self):
+        ec = emptycell.EmptyCell()
+        self.assertTrue(ec is ec.unwrap())
