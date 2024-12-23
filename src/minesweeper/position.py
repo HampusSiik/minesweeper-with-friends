d5d6468e58ec541e0b8dee453281e30f8ec30e03
Hampus Siik <hampus.siik@gmail.com>
2024-12-23 23:52:15 +0100

    Add from_dict function to convert dictionary to Position tuple

--- src/minesweeper/position.py
+++ src/minesweeper/position.py
@@ -5,3 +5,16 @@ Position = Tuple[int, int]
 """
 Represents a position on the board.
 """
+
+
+def from_dict(data: dict) -> Position:
+    """
+    Converts a dictionary to a Position tuple.
+
+    Args:
+        data (dict): The dictionary to convert.
+
+    Returns:
+        Position: The Position tuple.
+    """
+    return data["row"], data["col"]
