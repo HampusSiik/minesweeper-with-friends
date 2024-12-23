5a9539588f1c1fc61a5f7cddd339a871b7eb1016
Hampus Siik <hampus.siik@gmail.com>
2024-12-23 10:32:24 +0100

    Update package documentation to include position and game modules

--- src/minesweeper/__init__.py
+++ src/minesweeper/__init__.py
@@ -3,9 +3,15 @@ The minesweeper package.
 
 This package contains the minesweeper game implementation.
 
+Modules:
+
+    position: The position module.
+
 Subpackages:
 
     board: The board module.
 
     cells: The cell module.
+
+    game: The game module.
 """
