a3b383fcfb2f04263ec87e2616f1c5c4c3195250
Hampus Siik <hampus.siik@gmail.com>
2024-12-23 11:28:53 +0100

    Refactor main function to use generate_game and left_click_cell methods in Minesweeper class

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from minesweeper.game.minesweeper import Minesweeper
 
 def main():
     game = Minesweeper()
-    game.generate_board(5, 10)
-    game.place_mines(15, (3, 3))
+    game.generate_game(5, 10, 15)
+    game.left_click_cell((3, 3))
     print(game._board.show_nearby_mines())
 
 
