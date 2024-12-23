f3d66749d66f317360c0a35df65ef69cde4213fb
Hampus Siik <hampus.siik@gmail.com>
2024-12-23 14:51:54 +0100

    Enhance game interaction by allowing user input for cell actions and updating mine count display

--- src/main.py
+++ src/main.py
@@ -5,9 +5,17 @@ from minesweeper.game.minesweeper import Minesweeper
 
 def main():
     game = Minesweeper()
-    game.generate_game(5, 10, 15)
-    game.left_click_cell((3, 3))
-    print(game.get_show_board().show())
+    game.generate_game(5, 10, 10)
+    while not game.game_lost() and not game.game_won():
+        print(game.get_show_board().show())
+        row = int(input("Enter row: "))
+        col = int(input("Enter col: "))
+        button = input("Enter button: ")
+        if button == "l":
+            game.left_click_cell((row, col))
+        if button == "r":
+            game.right_click_cell((row, col))
+    print(game._board.show_nearby_mines())
 
 
 if __name__ == "__main__":
