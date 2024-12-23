c919c964c93bb4d80544568f86fd53c04b71b04f
Hampus Siik <hampus.siik@gmail.com>
2024-12-23 10:31:33 +0100

    Refactor Minesweeper class to use Board instance and enhance method documentation

--- src/minesweeper/game/minesweeper.py
+++ src/minesweeper/game/minesweeper.py
@@ -1,42 +1,99 @@
 from typing import Tuple
 from minesweeper.position import Position
-from minesweeper.board import board
+from minesweeper.board.board import Board
 
 
 class Minesweeper:
+    """
+    Minesweeper game.
+
+    """
+
+    _board: Board
+    """
+    Minesweeper board.
+    """
+
     def __init__(self):
-        self.board = board.Board()
+        """
+        Initialize the game.
+        """
+        self._board = Board()
 
     def shape(self) -> Tuple[int, int]:
-        return self.board.shape()
+        """
+        Get the shape of the board.
+
+        Returns:
+            Tuple[int, int]: Shape of the board.
+        """
+        return self._board.shape()
 
     def game_lost(self) -> bool:
+        """
+        Check if the game is lost.
+
+        Returns:
+            bool: True if the game is lost, False otherwise.
+        """
         return any(
-            self.board.is_mine(position) and self.board.is_revealed(position)
-            for position in self.board.all_positions()
+            self._board.is_mine(position) and self._board.is_revealed(position)
+            for position in self._board.all_positions()
         )
 
     def game_won(self) -> bool:
+        """
+        Check if the game is won.
+
+        Returns:
+            bool: True if the game is won, False otherwise.
+        """
         return (
             all(
-                self.board.is_mine(position) or self.board.is_revealed(position)
-                for position in self.board.all_positions()
+                self._board.is_mine(position) or self._board.is_revealed(position)
+                for position in self._board.all_positions()
             )
             and not self.game_lost()
         )
 
     def left_click_cell(self, position: Position) -> None:
-        if self.board.is_flagged(position):
+        """
+        Left click a cell.
+
+        Args:
+            position (Position): Position to left click.
+        """
+        if self._board.is_flagged(position):
             return
-        self.board.reveal(position)
+        self._board.reveal(position)
 
     def right_click_cell(self, position: Position) -> None:
-        if self.board.is_revealed(position):
+        """
+        Right click a cell.
+
+        Args:
+            position (Position): Position to right click.
+        """
+        if self._board.is_revealed(position):
             return
-        self.board.toggle_flag(position)
+        self._board.toggle_flag(position)
 
     def generate_board(self, rows: int, columns: int) -> None:
-        self.board.generate_board(rows, columns)
+        """
+        Generate a board.
 
-    def place_mines(self, num_mines: int, position: Position) -> None:
-        self.board.place_mines(num_mines, position)
+        Args:
+            rows (int): Number of rows.
+            columns (int): Number of columns.
+        """
+        self._board.generate_board(rows, columns)
+
+    def place_mines(self, num_mines: int, start_position: Position) -> None:
+        """
+        Place mines on the board.
+
+        Args:
+            num_mines (int): Number of mines to place.
+            start_position (Position): Position to avoid placing mines.
+        """
+        self._board.place_mines(num_mines, start_position)
