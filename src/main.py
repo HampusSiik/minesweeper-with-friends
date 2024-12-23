06b7e431c013698c4ec50535cac64560be4fbf5a
Hampus Siik <hampus.siik@gmail.com>
2024-12-24 00:59:07 +0100

    Update Flask app to use environment variables for host, port, and debug settings

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
+from os import environ
+
 from minesweeper_api.api_app import app, socketio
 
 
 def main():
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(
+        app,
+        host=environ.get("ADDRESS", "0.0.0.0"),
+        port=environ.get("PORT", 5000),
+        debug=environ.get("DEBUG", True),
+    )
 
 
 if __name__ == "__main__":
