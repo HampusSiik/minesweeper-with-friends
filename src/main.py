f6f4c69c03aa346bca512b9fd4012f8262601327
Hampus Siik <hampus.siik@gmail.com>
2024-12-23 11:02:00 +0100

    Refactor main function to use Minesweeper class for board generation and mine placement

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from minesweeper.board import board
+from minesweeper.game.minesweeper import Minesweeper
 
 
 def main():
-    b = board.Board()
-    b.generate_board(5, 7)
-    b.place_mines(5, (0, 0))
-    print(b.show_nearby_mines())
+    game = Minesweeper()
+    game.generate_board(5, 10)
+    game.place_mines(15, (3, 3))
+    print(game._board.show_nearby_mines())
 
 
 if __name__ == "__main__":
