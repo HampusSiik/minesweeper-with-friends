20878614fc6d0fa7b8137eca49347a8361d659b9
Hampus Siik <hampus.siik@gmail.com>
2025-01-06 14:20:31 +0100

    Add module docstring for Minesweeper API 2 to enhance documentation

---
+++ src/minesweeper_api2/__init__.py
@@ -0,0 +1,9 @@
+"""
+The second version of the Minesweeper API is a more complex version of the first one.
+
+The Minesweeper API 2 has a RoomAPI class that manages the rooms and players.
+
+It has methods to create a new room, join a room, leave a room, update the room options, and get the room state.
+
+The RoomAPI class also has a method to generate a new room ID.
+"""
