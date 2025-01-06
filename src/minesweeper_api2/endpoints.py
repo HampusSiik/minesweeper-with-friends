310c94fdf7600744a52e7d1e6289a03b23ae8c0d
Hampus Siik <hampus.siik@gmail.com>
2025-01-06 17:00:11 +0100

    Implement basic endpoints for game functionality including login, room creation, and room management

---
+++ src/minesweeper_api2/endpoints.py
@@ -0,0 +1,121 @@
+from typing import Dict, Optional, Any
+from flask import request, redirect
+
+from minesweeper_room.roomapi import GameOptions, Player
+from .api_app import app, session, room_api
+
+
+def _no_player() -> Any:
+    """
+    Handles the case where no player is set.
+    """
+
+    return redirect("/login")
+
+
+def _not_implemented() -> Any:
+    """
+    Handles the case where the endpoint is not implemented.
+    """
+
+    return {"error": "Not implemented."}, 501
+
+
+@app.route("/", methods=["GET"])
+def index() -> Any:
+    """
+    Get the index page.
+    """
+
+    return _not_implemented()
+
+
+@app.route("/login", methods=["POST"])
+def login() -> Any:
+    """
+    Set the player name.
+    """
+
+    request_data: Optional[Dict[str, str]] = request.json
+    if not request_data:
+        return {"error": "No data provided."}, 400
+
+    player = request_data.get("player")
+    if not player:
+        return {"error": "Invalid data provided."}, 400
+
+    session["player"] = Player(player)
+
+    return redirect("/")
+
+
+@app.route("/login", methods=["GET"])
+def get_login() -> Any:
+    """
+    Get the login page.
+    """
+
+    return _not_implemented()
+
+
+@app.route("/rooms", methods=["POST"])
+def create_room() -> Any:
+    """
+    Create a new room.
+    """
+
+    player = session.get("player")
+    if not player:
+        return _no_player()
+
+    request_data: Optional[Dict[str, str]] = request.json
+    if not request_data:
+        return {"error": "No data provided."}, 400
+
+    mines = request_data.get("mines")
+    cols = request_data.get("cols")
+    rows = request_data.get("rows")
+
+    if not mines or not cols or not rows:
+        return {"error": "Invalid data provided."}, 400
+
+    game_options = GameOptions(int(mines), int(cols), int(rows))
+
+    room_id = room_api.create_room(game_options)
+
+    return redirect(f"/rooms/{room_id}")
+
+
+# join_room
+@app.route("/rooms/<room_id>", methods=["GET"])
+def get_room(room_id: str) -> Any:
+    """
+    Get the room data.
+
+    Args:
+        room_id (str): The room ID.
+    """
+
+    player = session.get("player")
+    if not player:
+        return _no_player()
+
+    room_api.join_room(room_id, player)
+    return _not_implemented()
+
+
+# leave_room
+@app.route("/rooms/<room_id>", methods=["DELETE"])
+def leave_room(room_id: str) -> Any:
+    """
+    Leave the room.
+
+    Args:
+        room_id (str): The room ID.
+    """
+
+    player = session.get("player")
+    if player is not None:
+        room_api.leave_room(room_id, player)
+
+    return redirect("/")
