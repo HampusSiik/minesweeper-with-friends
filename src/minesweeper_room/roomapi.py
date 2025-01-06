402540ea36695a7c5f7ae89914bc92624c66cb88
Hampus Siik <hampus.siik@gmail.com>
2025-01-06 17:28:43 +0100

    Add observer pattern to RoomAPI for room state updates

--- src/minesweeper_room/roomapi.py
+++ src/minesweeper_room/roomapi.py
@@ -1,4 +1,5 @@
 import secrets
+from typing import Callable, List
 
 from .gameoptions import GameOptions
 from .roomregistry import RoomRegistry
@@ -16,11 +17,36 @@ class RoomAPI:
     The registry of rooms.
     """
 
+    _update_observers: List[Callable[[RoomState], None]]
+    """
+    The list of observers to notify when a room is updated.
+    """
+
     def __init__(self) -> None:
         """
         Initializes a new RoomAPI object.
         """
         self._room_registry = RoomRegistry()
+
+    def _notify_observers(self, room_id: str) -> None:
+        """
+        Notifies all observers that a room has been updated.
+
+        Args:
+            room_id (str): The ID of the room.
+        """
+        room_state = self._room_registry.get_room_state(room_id)
+        for observer in self._update_observers:
+            observer(room_state)
+
+    def add_update_observer(self, observer: Callable[[RoomState], None]) -> None:
+        """
+        Adds an observer to the list of observers.
+
+        Args:
+            observer (Callable[[RoomState], None]): The observer to add.
+        """
+        self._update_observers.append(observer)
 
     def create_room(self, options: GameOptions) -> str:
         """
@@ -57,6 +83,7 @@ class RoomAPI:
             player (Player): The player to add.
         """
         self._room_registry.add_player(room_id, player)
+        self._notify_observers(room_id)
 
     def leave_room(self, room_id: str, player: Player) -> None:
         """
@@ -67,6 +94,7 @@ class RoomAPI:
             player (Player): The player to remove.
         """
         self._room_registry.remove_player(room_id, player)
+        self._notify_observers(room_id)
 
     def update_room_options(self, room_id: str, options: GameOptions) -> None:
         """
@@ -77,6 +105,7 @@ class RoomAPI:
             options (GameOptions): The new options of the room.
         """
         self._room_registry.update_options(room_id, options)
+        self._notify_observers(room_id)
 
     def get_room_state(self, room_id: str) -> RoomState:
         """
